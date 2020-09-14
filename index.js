deac3da790e35242f0fae9d6043ff02f7db3ad41
cawilson1 <cawilson1@g.cofc.edu>
2020-09-14 11:36:07 -0400

    CRUD operations for a mongo users table/collection

--- index.js
+++ index.js
@@ -9,4 +9,85 @@ const connect = () => {
     { useNewUrlParser: true, useUnifiedTopology: true }
   );
 };
-connect().then(resp => console.log(resp));
+
+const user = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  //   password: { type: String, required: true },
+  firstName: String,
+  lastName: String,
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }]
+});
+
+const UserModel = mongoose.model("user", user);
+
+connect().then(connection => {
+  //create user
+  async function createUser() {
+    try {
+      const createdUser = await UserModel.create({
+        username: "del",
+        firstName: "del",
+        lastName: "del",
+        following: ["5f5f857173892c4408401e57", "5f5f85f887dcf2441f096960"]
+      });
+      console.log(createdUser);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //   createUser();
+
+  //list all users
+  async function listUsers() {
+    try {
+      const users = await UserModel.find();
+      console.log(users);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //   listUsers();
+
+  async function getOneUser() {
+    try {
+      const user = await UserModel.findById("5f5f85f887dcf2441f096960");
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //   getOneUser();
+
+  async function updateOneUser() {
+    try {
+      const user = await UserModel.findByIdAndUpdate(
+        "5f5f857173892c4408401e57",
+        { lastName: "Sunglasses", following: ["5f5f85f887dcf2441f096960"] }
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //   updateOneUser();
+
+  async function deleteById() {
+    try {
+      const deletedUser = await UserModel.findByIdAndDelete(
+        "5f5f8c4822d73244cf6e9995"
+      );
+      console.log(deletedUser);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //   deleteById();
+
+  //   async listFollowing(){
+  //       try {
+
+  //       } catch (error) {
+  //           console.log(error)
+  //       }
+  //   }
+});
